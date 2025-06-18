80401dbda7a23b5523640f5540ed109c36dfbada
MOD Administrator <admin@MngEnvMCAP732343.onmicrosoft.com>
2025-06-18 17:36:34 +0700

    today

--- ServiceBusSender/Program.cs
+++ ServiceBusSender/Program.cs
@@ -1,34 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Program.cs
+using System;
+using System.Threading.Tasks;
+using ServiceBusSenderModule; // This using statement covers both SenderModule and Employee
 
-using Azure.Messaging.ServiceBus;
-//using System.Text.Json.Serialization;
-//using Newtonsoft.Json;
-//using System.Text;
-using System.Text.Json;
-string connectionString = "Endpoint=sb://mentossrvbus.servicebus.windows.net/;SharedAccessKeyName=queuepolicy;SharedAccessKey=YGl/cJ+F7sejJKEHfCZ0SHEZOBGd4Xxvs+ASbNPBePg=;EntityPath=firstqueue";
-string queueName = "firstqueue";
-
-// service bus client
-ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
-
-// service bus sender
-ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(queueName);
-
-// send message using service bus sender
-//ServiceBusMessage serviceBusMessage = new ServiceBusMessage("hi from vs");
-//await serviceBusSender.SendMessageAsync(serviceBusMessage);
-Employee employee = new Employee() { FirstName = "Skill", LastName = "Mellon", Salary = 27000 };
-//var employeeDataInJson = JsonConvert.SerializeObject(employee);
-//var msg = new ServiceBusMessage(Encoding.UTF8.GetBytes(employeeDataInJson));
-string jsonString = JsonSerializer.Serialize(employee);
-var msg = new ServiceBusMessage(jsonString);
-
-await serviceBusSender.SendMessageAsync(msg);
-
-Console.WriteLine("message sent!");
-class Employee
+public class Program
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public int Salary { get; set; }
+    private const string ConnectionString = "Endpoint=sb://mentossrvbus.servicebus.windows.net/;SharedAccessKeyName=queuepolicy;SharedAccessKey=YGl/cJ+F7sejJKEHfCZ0SHEZOBGd4Xxvs+ASbNPBePg=;EntityPath=firstqueue";
+    private const string QueueName = "firstqueue";
+
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Service Bus Sender Application Started.");
+
+        await using var senderModule = new SenderModule(ConnectionString, QueueName);
+
+        try
+        {
+            await senderModule.InitializeAsync();
+
+            Console.WriteLine("\n--- Sending Messages ---");
+
+            await senderModule.SendStringMessageAsync("hi from vs (string)");
+
+            // Employee is now in its own file, but still accessible via the 'using ServiceBusSenderModule;'
+            var employee1 = new Employee() { FirstName = "Skill", LastName = "Mellon", Salary = 27000 };
+            await senderModule.SendObjectAsJsonMessageAsync(employee1);
+
+            var employee2 = new Employee() { FirstName = "Jane", LastName = "Doe", Salary = 35000 };
+            await senderModule.SendObjectAsJsonMessageAsync(employee2);
+
+            await senderModule.SendStringMessageAsync("Another simple message");
+
+            Console.WriteLine("\nAll messages sent!");
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"An unhandled error occurred in Program.cs: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Service Bus Sender Application Finished.");
+        }
+    }
 }
--- ServiceBusSender/obj/Debug/net8.0/ServiceBusSender.AssemblyInfo.cs
+++ ServiceBusSender/obj/Debug/net8.0/ServiceBusSender.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceBusSender")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9bf6908d4f63545059439b01bccb199ab54160")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceBusSender")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceBusSender")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
